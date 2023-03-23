15bb28c0ba493dd8e52320b6e3039d48971c27c9
5kyBuilder <124388396+5kyBuilder@users.noreply.github.com>
2023-03-23 15:22:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function youtubeclick()
+{
+    document.getElementById("youtube").style.cursor = "pointer";
+    window.open("https://www.youtube.com/@bhavanspublicschooldoha-qa158");
+}
+
+function instagramclick()
+{
+    document.getElementById("instagram").style.cursor = "pointer";
+    window.open("https://www.instagram.com/bhavansqatar/?hl=en");
+}
+
+function facebookclick()
+{
+    document.getElementById("facebook").style.cursor = "pointer";
+    window.open("https://www.facebook.com/bhavansqatar/");
+}
+
+function schoolclick()
+{
+    document.getElementById("school").style.cursor = "pointer";
+    window.open("https://bhavansqatar.org/");
+}
